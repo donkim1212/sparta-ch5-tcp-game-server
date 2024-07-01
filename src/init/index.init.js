375c8377e2a2dc79ed0da3fb14ea9638e150873d
Dongkyun Kim <donkim0122@gmail.com>
2024-07-01 15:27:18 +0900

    Add: proto.init.js that loads proto files and map them into 'protoMessages' object Update: index.init.js calls loadProtoFiles() from proto.init.js

--- src/init/index.init.js
+++ src/init/index.init.js
@@ -1,6 +1,9 @@
+import { loadProtoFiles } from "./proto.init.js";
+
 export const initServer = async () => {
   try {
     // load assets / proto files here
+    await loadProtoFiles();
   } catch (err) {
     //
     console.error(err);
