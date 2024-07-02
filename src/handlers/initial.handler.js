0cd1f66ea83ab20434a87cda10fbc953047bdef8
Dongkyun Kim <donkim0122@gmail.com>
2024-07-02 19:56:43 +0900

    Update: initialHandler adds user to userSessions & game session

--- src/handlers/initial.handler.js
+++ src/handlers/initial.handler.js
@@ -1,7 +1,11 @@
-const initialHandler = async ({ socket, userId, payload }) => {
-  const { deviceId } = payload;
+import gameSessionsManager from "../session/game.session.js";
+import userSessionsManager from "../session/user.session.js";
 
-  //   socket.write("");
+const initialHandler = async ({ socket, userId, payload }) => {
+  const { deviceId, playerId } = payload; // deviceId IS userId
+
+  const game = gameSessionsManager.getGameSession(0);
+  game.addUser(userSessionsManager.addUser(deviceId, playerId, socket));
 };
 
 export default initialHandler;
