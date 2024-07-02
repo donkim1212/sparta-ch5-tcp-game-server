b9add1fe68a26623efd2895b89ea9e4775b94d9f
Dongkyun Kim <donkim0122@gmail.com>
2024-07-02 19:52:55 +0900

    Update: add a game session on server init

--- src/init/index.init.js
+++ src/init/index.init.js
@@ -1,9 +1,11 @@
+import gameSessionsManager from "../session/game.session.js";
 import { loadProtoFiles } from "./proto.init.js";
 
 export const initServer = async () => {
   try {
     // load assets / proto files here
     await loadProtoFiles();
+    gameSessionsManager.addGameSession(0);
   } catch (err) {
     //
     console.error(err);
