7b2bbf3c7deaefc6b9714cbe372560ffff7c1aa2
Dongkyun Kim <donkim0122@gmail.com>
2024-07-02 19:54:50 +0900

    Update: remove user from userSessions and gameSessions on disconnection with 'End' event

--- src/events/end.event.js
+++ src/events/end.event.js
@@ -1,6 +1,17 @@
+import gameSessionsManager from "../session/game.session.js";
+import { userSessions } from "../session/session.js";
+import userSessionsManager from "../session/user.session.js";
+
 export const onEnd = (socket) => () => {
   try {
     console.log(`Client disconnected: ${socket.remoteAddress}:${socket.remotePort}`);
+    for (const [userId, user] of Object.entries(userSessions)) {
+      if (user.socket === socket) {
+        userSessionsManager.removeUserByuserId(userId);
+        gameSessionsManager.getGameSession(0).removeUser(userId);
+        break;
+      }
+    }
   } catch (err) {
     //
   }
