32410f325a89ffcb09f47989580724dc97747fd8
Dongkyun Kim <donkim0122@gmail.com>
2024-07-02 19:58:01 +0900

    Update: locationUpdateHandler now updates user's position data on server memory, and sends other users' location data back to client

--- src/handlers/location-update.handler.js
+++ src/handlers/location-update.handler.js
@@ -1,5 +1,28 @@
+import { headerConstants } from "../constants/header.constants.js";
+import { getProtoMessages } from "../init/proto.init.js";
+import gameSessionsManager from "../session/game.session.js";
+import userSessionsManager from "../session/user.session.js";
+import { writeHeader } from "../utils/header.utils.js";
+
 const locationUpdateHandler = async ({ socket, userId, payload }) => {
-  //
+  const { x, y } = payload;
+  const user = userSessionsManager.getUserByUserId(userId);
+  /* do some calculation for x, y validation here */
+  user.updatePosition(x, y);
+
+  const LocationUpdate = getProtoMessages().game.LocationUpdate;
+  const game = gameSessionsManager.getGameSession(0);
+
+  const filteredUsers = game.getAllUsers().filter((user) => user.id !== userId);
+
+  const err = LocationUpdate.verify({ users: filteredUsers });
+  if (err) {
+    console.error(err);
+  }
+
+  const responsePayload = LocationUpdate.encode({ users: filteredUsers }).finish();
+  const header = writeHeader(responsePayload.length, headerConstants.packetTypes.LOCATION);
+  return Buffer.concat([header, responsePayload]);
 };
 
 export default locationUpdateHandler;
