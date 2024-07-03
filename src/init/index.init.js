b56fec46af59f89f0ef6b6241a1deb6c0548c4f5
Dongkyun Kim <donkim0122@gmail.com>
2024-07-03 16:21:17 +0900

    Update: add DB test function call

--- src/init/index.init.js
+++ src/init/index.init.js
@@ -1,10 +1,13 @@
+import pools from "../db/connect.db.js";
 import gameSessionsManager from "../session/game.session.js";
+import { testAllDbConnections } from "../utils/db/test-db-connection.js";
 import { loadProtoFiles } from "./proto.init.js";
 
 export const initServer = async () => {
   try {
     // load assets / proto files here
     await loadProtoFiles();
+    await testAllDbConnections(pools);
     gameSessionsManager.addGameSession(0);
   } catch (err) {
     //
