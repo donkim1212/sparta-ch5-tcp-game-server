06da57c42d74120ff5f6486f752b24384bb0882a
Dongkyun Kim <donkim0122@gmail.com>
2024-07-03 17:45:06 +0900

    Update: set socket timeout (10 secs) Update: 'onTimeout' remove user from game/user sessions, and update user's last location

--- src/events/timeout.event.js
+++ src/events/timeout.event.js
@@ -1,9 +1,15 @@
 import gameSessionsManager from "../session/game.session.js";
 import userSessionsManager from "../session/user.session.js";
+import { handleError } from "../utils/errors/error-handler.js";
 
-export const onTimeout = (socket) => () => {
-  // disconnect socket and remove user(?) on timeout
-  const game = gameSessionsManager.getGameSession(0);
-  //   const user = userSessionsManager.
-  //   game.removeUser();
+export const onTimeout = (socket) => async () => {
+  try {
+    console.log(`Client disconnected (timeout): ${socket.remoteAddress}:${socket.remotePort}`);
+    const user = userSessionsManager.getUserBySocket(socket);
+    await userDbQueries.updateUserLogin(user.id, user.x, user.y);
+    userSessionsManager.removeUserByuserId(user.id);
+    gameSessionsManager.getGameSession(0).removeUser(user.id);
+  } catch (err) {
+    handleError(socket, err);
+  }
 };
