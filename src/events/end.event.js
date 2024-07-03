fbf4cced714ad81b03c98e0272abc1428b0b5599
Dongkyun Kim <donkim0122@gmail.com>
2024-07-03 16:23:17 +0900

    Update: on 'End' event, update user's last position in DB

--- src/events/end.event.js
+++ src/events/end.event.js
@@ -1,3 +1,4 @@
+import userDbQueries from "../db/user.db.js";
 import gameSessionsManager from "../session/game.session.js";
 import { userSessions } from "../session/session.js";
 import userSessionsManager from "../session/user.session.js";
@@ -7,6 +8,8 @@ export const onEnd = (socket) => () => {
     console.log(`Client disconnected: ${socket.remoteAddress}:${socket.remotePort}`);
     for (const [userId, user] of Object.entries(userSessions)) {
       if (user.socket === socket) {
+        userDbQueries.updateUserLogin();
+        user.x;
         userSessionsManager.removeUserByuserId(userId);
         gameSessionsManager.getGameSession(0).removeUser(userId);
         break;
