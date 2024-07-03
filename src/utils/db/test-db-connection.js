3c61e731605e1ef6ab96b83e9733fcac296835ac
Dongkyun Kim <donkim0122@gmail.com>
2024-07-03 16:20:30 +0900

    Add: tester function to test DBs in pools

---
+++ src/utils/db/test-db-connection.js
@@ -0,0 +1,14 @@
+const testDbConnection = async (pool, dbName) => {
+  try {
+    const [rows] = await pool.query(`SELECT 1 + 1 AS solution`);
+    console.log(`${dbName} | Test query Result: ${rows[0].solution}`);
+  } catch (err) {
+    console.error(`${dbName} | Test query ERROR: ${err}`);
+  }
+};
+
+const testAllDbConnections = async (pools) => {
+  await testDbConnection(pools.USER_DB, "USER_DB");
+};
+
+export { testDbConnection, testAllDbConnections };
