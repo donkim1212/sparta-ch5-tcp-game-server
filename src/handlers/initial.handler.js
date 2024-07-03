ba1dcaafae7613b6815da9ee37540f36000e3ea3
Dongkyun Kim <donkim0122@gmail.com>
2024-07-03 16:22:26 +0900

    Update: initial handler creates user's data with given info

--- src/handlers/initial.handler.js
+++ src/handlers/initial.handler.js
@@ -1,11 +1,13 @@
 import gameSessionsManager from "../session/game.session.js";
 import userSessionsManager from "../session/user.session.js";
+import userDbQueries from "../db/user.db.js";
 
 const initialHandler = async ({ socket, userId, payload }) => {
   const { deviceId, playerId } = payload; // deviceId IS userId
 
   const game = gameSessionsManager.getGameSession(0);
   game.addUser(userSessionsManager.addUser(deviceId, playerId, socket));
+  userDbQueries.createUser(deviceId, 0, 0);
 };
 
 export default initialHandler;
