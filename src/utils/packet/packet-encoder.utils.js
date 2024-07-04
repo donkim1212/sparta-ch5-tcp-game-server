6fad1158501ee7986cc3f01b80a716b8f3106b68
Dongkyun Kim <donkim0122@gmail.com>
2024-07-04 19:04:30 +0900

    Feat: 'deserialize' that decodes data using proto message Update: rename 'packetEncoder' to 'serialize'

--- src/utils/packet/packet-encoder.utils.js
+++ src/utils/packet/packet-encoder.utils.js
@@ -1,14 +1,16 @@
 import { errorCodes } from "../../constants/error.constants.js";
 import { getProtoMessages } from "../../init/proto.init.js";
 import CustomError from "../errors/classes/custom.error.js";
+import { writeHeader } from "./header.utils.js";
 
 /**
  *
  * @param {string} type name of proto message, e.g., 'common.CommonPacket'
  * @param {Object} data to be encoded
+ * @param {number} headerTypeName one of protoTypeNames.packageName.typeNames
  * @returns packet encoded using protobuf message
  */
-const packetEncoder = (type, data) => {
+export const serialize = (type, data, headerTypeName) => {
   const [namespace, typeName] = type.split(".");
   const PayloadType = getProtoMessages()[namespace][typeName];
 
@@ -18,8 +20,24 @@ const packetEncoder = (type, data) => {
     throw new CustomError(errorCodes.PACKET_STRUCTURE_MISMATCH, "Error on verifying packet.");
   }
   const encoded = PayloadType.encode(data).finish();
+  if (headerTypeName) {
+    const header = writeHeader(encoded.length, headerTypeName);
+    return Buffer.concat([header, encoded]);
+  }
 
   return encoded;
 };
+/**
+ *
+ * @param {string} type name of proto message, e.g., 'common.CommonPacket'
+ * @param {Object} data to be decoded
+ * @returns decoded data
+ */
+export const deserialize = (type, data) => {
+  const [namespace, typeName] = type.split(".");
+  const PayloadType = getProtoMessages()[namespace][typeName];
 
-export default packetEncoder;
+  const decoded = PayloadType.decode(data);
+
+  return decoded;
+};
