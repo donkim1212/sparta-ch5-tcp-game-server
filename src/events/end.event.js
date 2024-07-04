93e2761abdea4d626865790ee1876627a8ea0b38
Dongkyun Kim <donkim0122@gmail.com>
2024-07-04 16:21:02 +0900

    Update: 'end' and 'timeout' use gameSession ID from game constants

--- src/events/end.event.js
+++ src/events/end.event.js
@@ -1,3 +1,4 @@
+import { MAIN_GAME_ID } from "../constants/game.constants.js";
 import userDbQueries from "../db/user.db.js";
 import gameSessionsManager from "../session/game.session.js";
 import userSessionsManager from "../session/user.session.js";
@@ -9,7 +10,7 @@ export const onEnd = (socket) => async () => {
     const user = userSessionsManager.getUserBySocket(socket);
     await userDbQueries.updateUserLogin(user.id, user.x, user.y);
     userSessionsManager.removeUserByuserId(user.id);
-    gameSessionsManager.getGameSession(0).removeUser(user.id);
+    gameSessionsManager.getGameSession(MAIN_GAME_ID).removeUser(user.id);
   } catch (err) {
     handleError(socket, err);
   }
