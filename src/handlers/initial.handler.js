c20f434cc173106c4e4ad178ec46655b0ced93f9
Dongkyun Kim <donkim0122@gmail.com>
2024-07-04 12:36:24 +0900

    Update: initialHandler now returns GameStart packet

--- src/handlers/initial.handler.js
+++ src/handlers/initial.handler.js
@@ -1,6 +1,11 @@
 import gameSessionsManager from "../session/game.session.js";
 import userSessionsManager from "../session/user.session.js";
 import userDbQueries from "../db/user.db.js";
+import packetEncoder from "../utils/packet/packet-encoder.utils.js";
+import GameStartData from "../protobuf/gameNotification/game-start.proto.js";
+import { protoTypeNames } from "../constants/proto.constants.js";
+import { writeHeader } from "../utils/packet/header.utils.js";
+import { headerConstants } from "../constants/header.constants.js";
 
 const initialHandler = async ({ socket, userId, payload }) => {
   const { deviceId, playerId } = payload; // deviceId IS userId
@@ -11,6 +16,12 @@ const initialHandler = async ({ socket, userId, payload }) => {
 
   /* create user if dne */
   await userDbQueries.createUser(deviceId, 0, 0);
+  const userData = await userDbQueries.findUserByDeviceId(deviceId);
+
+  const data = new GameStartData(game.id, userData.x, userData.y);
+  const serialized = packetEncoder(protoTypeNames.gameNotification.GameStart, data);
+  const header = writeHeader(serialized.length, headerConstants.packetTypes.GAME_START);
+  return Buffer.concat([header, serialized]);
 };
 
 export default initialHandler;
