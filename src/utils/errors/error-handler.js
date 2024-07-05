1f35cc690723d2ecb61f90db485b9fb7f0d2cd9b
Dongkyun Kim <donkim0122@gmail.com>
2024-07-05 22:45:06 +0900

    Update: add error handling per error codes

--- src/utils/errors/error-handler.js
+++ src/utils/errors/error-handler.js
@@ -1,9 +1,24 @@
+import { errorCodes } from "../../constants/error.constants.js";
+import { userRemover } from "../user-remover.js";
+
 export const handleError = (socket, error) => {
   try {
     if (error.code) {
       console.error(`Code: ${error.code}, Message: ${error.message}`);
-    } else {
-      console.error(error);
+    }
+    switch (error.code) {
+      case undefined: {
+        console.error(error);
+        break;
+      }
+      case errorCodes.SOCKET_ERROR: {
+        userRemover(socket);
+        break;
+      }
+      default: {
+        userRemover(socket);
+        break;
+      }
     }
 
     // send response packet here
